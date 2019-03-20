e2ef9a52f19dc2bb8ce02404f554781e96435d18
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-03-20 20:21:17 +0100

    add build size codecheck

--- codechecks.ts
+++ codechecks.ts
@@ -1,38 +1,62 @@
+import { buildSize } from "codecheck-build-size";
 import { codeChecks } from "codechecks";
 
+// disabling faulty rules
+// tslint:disable:no-useless-cast restrict-plus-operands
 export async function main(): Promise<void> {
-  checkPackageLock();
-  checkSmartContractGitModules();
+  await checkPackageLock();
+  await checkSmartContractGitModules();
+
+  await buildSize({
+    files: [
+      {
+        path: "dist/*.vendors*.js",
+        maxSize: "1000kB",
+      },
+      {
+        path: "dist/*.main*.js",
+        maxSize: "350kB",
+      },
+      {
+        path: "dist/*.main*.css",
+        maxSize: "60kB",
+      },
+      {
+        path: "dist/*.vendors*.css",
+        maxSize: "20kB",
+      },
+    ],
+  });
 }
 
-function checkPackageLock() {
+async function checkPackageLock(): Promise<void> {
   if (!codeChecks.isPr()) {
     return;
   }
-  const hasPackageLock = codeChecks.context.pr.files.added.includes("package-lock.json");
+  const hasPackageLock = codeChecks.context.pr!.files.added.includes("package-lock.json");
 
   if (hasPackageLock) {
-    codeChecks.failure({
+    await codeChecks.failure({
       name: "Package lock detected",
       shortDescription: "Do not use npm, use yarn instead.",
     });
   }
 }
 
-function checkSmartContractGitModules() {
+async function checkSmartContractGitModules(): Promise<void> {
   if (!codeChecks.isPr()) {
     return;
   }
   const updatesContracts = (
-    codeChecks.context.pr.meta.body + codeChecks.context.pr.meta.title
+    codeChecks.context.pr!.meta.body + codeChecks.context.pr!.meta.title
   ).includes("#with-contracts");
 
-  const hasContractSubmodule = codeChecks.context.pr.files.changed.some(p =>
+  const hasContractSubmodule = codeChecks.context.pr!.files.changed.some(p =>
     p.includes("platform-contracts-artifacts/"),
   );
 
   if (hasContractSubmodule && !updatesContracts) {
-    codeChecks.failure({
+    await codeChecks.failure({
       name: "Smart Contracts Submodule",
       shortDescription:
         "Detected platform-contracts-artifacts in your PR, most likely this is by mistake please push alone",
