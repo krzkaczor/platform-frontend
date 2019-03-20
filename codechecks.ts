60284567c469821b7115b3d823fffbb7a3d0ac4b
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-03-20 23:20:32 +0100

    add visual regression check

--- codechecks.ts
+++ codechecks.ts
@@ -1,5 +1,9 @@
 import { buildSize } from "codecheck-build-size";
 import { codeChecks } from "codechecks";
+import { join } from "path";
+
+// tslint:disable-next-line
+const exec = require("await-exec") as (cmd: string, opt: any) => Promise<void>;
 
 // disabling faulty rules
 // tslint:disable:no-useless-cast restrict-plus-operands
@@ -27,6 +31,29 @@ export async function main(): Promise<void> {
       },
     ],
   });
+
+  await visReg();
+}
+
+async function visReg(): Promise<void> {
+  const execOptions = { timeout: 300000, cwd: process.cwd(), log: true };
+  await codeChecks.saveCollection("storybook-vis-reg", join(__dirname, "__screenshots__"));
+
+  if (codeChecks.isPr()) {
+    await codeChecks.getCollection("storybook-vis-reg", join(__dirname, ".reg/expected"));
+    await exec("./node_modules/.bin/reg-suit compare", execOptions);
+
+    await codeChecks.saveCollection("storybook-vis-reg-report", join(__dirname, ".reg"));
+
+    const reportData = require("./.reg/out.json");
+    await codeChecks.success({
+      name: "Visual regression for Storybook",
+      shortDescription: `Changed: ${reportData.failedItems.length}, New: ${
+        reportData.newItems.length
+      }, Deleted: ${reportData.deletedItems.length}`,
+      detailsUrl: codeChecks.getArtifactLink("/storybook-vis-reg-report/index.html"),
+    });
+  }
 }
 
 async function checkPackageLock(): Promise<void> {
