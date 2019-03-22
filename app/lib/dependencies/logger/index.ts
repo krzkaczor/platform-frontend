be527d331e1c405899f9c872b9ea7c22168d4655
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-03-22 07:47:51 +0100

    DROP enable logging

--- app/lib/dependencies/logger/index.ts
+++ app/lib/dependencies/logger/index.ts
@@ -17,13 +17,6 @@ const resolveLogger = () => {
     return new SentryLogger(process.env.NF_SENTRY_DSN);
   }
 
-  if (process.env.NODE_ENV === "production") {
-    // tslint:disable-next-line
-    console.info("Error logging is disabled");
-
-    return noopLogger;
-  }
-
   return new DevConsoleLogger();
 };
 
